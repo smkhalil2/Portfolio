1cbccd4d4e798ff0b6a6d4e7a0624fa40c21eeda
Shadi <shadi.khalil98@gmail.com>
2023-08-15 19:02:00 -0400

    Profile pic changed to Next/Image

--- portfolio/src/components/Hero.jsx
+++ portfolio/src/components/Hero.jsx
@@ -1,19 +1,22 @@
+import Image from "next/image";
 import Skills from "./Skills";
 
 const Hero = () => {
   return (
     <div className="flex flex-col">
     <div className="flex lg:flex-row flex-col items-center bg-[url('/images/Spider-Gwen-PhotoRoom.png')] bg-cover backdrop-blur-sm">
-      <div className="flex flex-col lg:px-28 px-52 lg:mt-0 mt-8 w-[150%] ">
+      <div className="flex flex-col lg:px-28 px-8 lg:mt-0 mt-8 w-[100%] ">
         <h2 className="font-extrabold text-4xl text-[#90fcef]">Hi, I&#39;m Shadi</h2>
         <p className='text-xl'>
           I combine my skills as a software developer with my creativity to design and build web content, applications, and games!
         </p>
       </div>
-      <div className='mr-4'>
-        <img
+      <div className='flex justify-center lg:mx-8 '>
+        <Image
+          width={1200}
+          height={1200}
           src="/images/profile-pic.jpg"
-          className="w-[55%] lg:ml-32 ml-24 my-10 rounded-full ring-8 ring-[#0F0F0F] hover:ring-offset-4 hover:ring-offset-[#90fcef] items-center"
+          className="lg:w-[65%] w-[50%] my-10 rounded-full ring-8 ring-[#0F0F0F] hover:ring-offset-4 hover:ring-offset-[#90fcef] "
           alt="Shadi's personal headshot"
         />
       </div>
