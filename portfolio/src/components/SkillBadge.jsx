7e08766c66f10829bafe0d6445b36ba0f332e2f3
Shadi <shadi.khalil98@gmail.com>
2023-10-01 14:22:29 -0400

    more thumbnails

--- portfolio/src/components/SkillBadge.jsx
+++ portfolio/src/components/SkillBadge.jsx
@@ -9,7 +9,7 @@ const SkillBadge = ({ name, logo, color, img_classes }) => {
   const [isFocused, setFocus] = useState(false);
   return (
     <div
-      className={`flex w-auto h-auto p-3 lg:m-4 m-2 rounded-full 
+      className={`flex w-auto h-auto p-3 lg:m-4 m-1 rounded-full 
               items-center justify-center shadow-inner shadow-gray-800 dark:shadow-black`}
       onMouseEnter={() => setFocus(true)}
       onMouseLeave={() => setFocus(false)}
