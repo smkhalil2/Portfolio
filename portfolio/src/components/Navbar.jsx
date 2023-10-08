5fc7cd165b5e7762c82dd8d7c117d8c2b8fd73ae
Shadi <shadi.khalil98@gmail.com>
2023-10-08 16:26:40 -0400

    resume link

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { useState, useContext } from "react";
 import Link from "next/link";
 import ThemeContext from "@/theme/ThemeContext";
 import Image from "next/image";
-const resumeUrl = "/assets/Khalil_Resume_Jul23_3.pdf";
+const resumeUrl = "/assets/Khalil_Resume_Oct23.pdf";
 
 const Navbar = () => {
   const { dark, toggleDark } = useContext(ThemeContext);
